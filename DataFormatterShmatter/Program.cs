15f1fb8490e1192f8870fabb12af909b8ec54b11
ckd4 <107938367+ckd4@users.noreply.github.com>
2022-12-28 05:13:01 +0300

    Add files via upload

---
+++ DataFormatterShmatter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DateFormatter;
+
+namespace DataFormatterShmatter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine(Formatter.Normalize(Console.ReadLine()) + "\n");
+            }
+            
+        }
+    }
+}
---
+++ DateFormatter/Formatter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace DateFormatter
+{
+    public static class Formatter
+    {
+        public static string Normalize(string text)
+        {
+            Regex reg1 = new Regex(@"\d*\d.\d*\d.\d\d*\d*\d");
+            Regex reg2 = new Regex(@"\d*\d.\w+.\d*\d");
+            if (reg1.IsMatch(text))
+            {
+                DateTime date = DateTime.Parse(reg1.Match(text).Value);
+                return $"{date.Day.ToString().PadLeft(2, '0')}." +
+                       $"{date.Month.ToString().PadLeft(2, '0')}." +
+                       $"{date.Year.ToString().PadLeft(2, '0')}";
+            }
+            else
+            {
+                return text + " года";
+            }
+        }
+    }
+}
